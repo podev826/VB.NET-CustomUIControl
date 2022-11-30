14925f384d03ef6069d065a783dd98f4cc359d8a
Po <po@po.com>
2022-12-01 00:13:06 +0700

    update

--- MetroTextBox.vb
+++ MetroTextBox.vb
@@ -9,10 +9,10 @@ Public Class MetroTextBox
     End Sub
 
     Private m_borderColor As Color
-    Private m_borderThickness As Integer
+    Private m_borderThickness As UInteger
     Private m_shadowColor As Color
     Private m_shadowThickness As Integer
-    Private m_borderRound As Integer
+    Private m_borderRound As UInteger
 
 
     Public Property BorderColor As Color
@@ -61,75 +61,40 @@ Public Class MetroTextBox
         End Set
     End Property
 
-
     Protected Overrides Sub OnPaint(e As System.Windows.Forms.PaintEventArgs)
         Dim g As Graphics = Parent.CreateGraphics
-        Dim p As Pen = New Pen(m_borderColor, m_borderThickness)
-        If (m_borderThickness > 0) Then
-            If (m_borderRound > 0) Then
-                DrawRoundedRectangle(
-                    New Pen(m_borderColor, m_borderThickness),
-                    g,
+        g.SmoothingMode = Drawing2D.SmoothingMode.HighQuality
+        Dim diam As Single = Math.Min(m_borderRound, Math.Min(Height, Width))
+        Dim r As New Rectangle(
                     CSng(Location.X - m_borderThickness / 2),
                     CSng(Location.Y - m_borderThickness / 2),
                     Width + m_borderThickness,
-                    Height + m_borderThickness,
-                    m_borderRound
-                )
-            Else
-                g.DrawRectangle(
-                    New Pen(m_borderColor, m_borderThickness),
-                    CSng(Location.X - m_borderThickness / 2),
-                    CSng(Location.Y - m_borderThickness / 2),
-                    Width + m_borderThickness,
-                    Height + m_borderThickness
-                )
-            End If
-        End If
+                    Height + m_borderThickness)
+        Dim path As Drawing2D.GraphicsPath = RoundedRectangle(r, diam)
+
+        Dim shadow As New Rectangle(
+                    CSng(Location.X - m_borderThickness + m_shadowThickness),
+                    CSng(Location.Y - m_borderThickness + m_shadowThickness),
+                    Width + 2 * m_borderThickness,
+                    Height + 2 * m_borderThickness)
+        Dim shadowPath As Drawing2D.GraphicsPath = RoundedRectangle(shadow, 2 * diam)
+        g.FillPath(New SolidBrush(m_shadowColor), shadowPath)
         MyBase.OnPaint(e)
+        g.DrawPath(New Pen(m_borderColor, m_borderThickness), path)
+
     End Sub
+    Private Function RoundedRectangle(rect As RectangleF, diam As Single) As Drawing2D.GraphicsPath
+        Dim path As New Drawing2D.GraphicsPath
+        If (diam > 0) Then
+            path.AddArc(rect.Left, rect.Top, diam, diam, 180, 90)
+            path.AddArc(rect.Right - diam, rect.Top, diam, diam, 270, 90)
+            path.AddArc(rect.Right - diam, rect.Bottom - diam, diam, diam, 0, 90)
+            path.AddArc(rect.Left, rect.Bottom - diam, diam, diam, 90, 90)
+            path.CloseFigure()
+        Else
+            path.AddRectangle(rect)
+        End If
+        Return path
+    End Function
 
-    Public Sub DrawRoundedRectangle(ByVal p As Pen,
-                                ByVal objGraphics As Graphics,
-                                ByVal m_intxAxis As Single,
-                                ByVal m_intyAxis As Single,
-                                ByVal m_intWidth As Single,
-                                ByVal m_intHeight As Single,
-                                ByVal m_diameter As Single)
-        'Dim g As Graphics
-        Dim BaseRect As New RectangleF(m_intxAxis, m_intyAxis, m_intWidth,
-                                      m_intHeight)
-
-        m_diameter = Math.Min(m_diameter, Math.Min(m_intHeight, m_intWidth))
-
-        objGraphics.DrawLine(p, m_intxAxis + CSng(m_diameter / 2),
-                             m_intyAxis,
-                             m_intxAxis + m_intWidth - CSng(m_diameter / 2),
-                             m_intyAxis)
-        objGraphics.DrawLine(p, m_intxAxis + m_intWidth,
-                             m_intyAxis + CSng(m_diameter / 2),
-                             m_intxAxis + m_intWidth,
-                             m_intyAxis + m_intHeight - CSng(m_diameter / 2))
-        objGraphics.DrawLine(p, m_intxAxis + CSng(m_diameter / 2),
-                             m_intyAxis + m_intHeight,
-                             m_intxAxis + m_intWidth - CSng(m_diameter / 2),
-                             m_intyAxis + m_intHeight)
-        objGraphics.DrawLine(p,
-                             m_intxAxis, m_intyAxis + CSng(m_diameter / 2),
-                             m_intxAxis,
-                             m_intyAxis + m_intHeight - CSng(m_diameter / 2))
-        Dim ArcRect As New RectangleF(BaseRect.Location,
-                                      New SizeF(m_diameter, m_diameter))
-        'top left Arc
-        objGraphics.DrawArc(p, ArcRect, 180, 90)
-        ' top right arc
-        ArcRect.X = BaseRect.Right - m_diameter
-        objGraphics.DrawArc(p, ArcRect, 270, 90)
-        ' bottom right arc
-        ArcRect.Y = BaseRect.Bottom - m_diameter
-        objGraphics.DrawArc(p, ArcRect, 0, 90)
-        ' bottom left arc
-        ArcRect.X = BaseRect.Left
-        objGraphics.DrawArc(p, ArcRect, 90, 90)
-    End Sub
 End Class
